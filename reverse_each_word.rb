5bc22484620695ec5078ccd0735cda581ff1be16
Robert Defendini <robdefendini@gmail.com>
2020-05-27 22:10:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,11 @@ return_array << string.reverse
   return_array.join("")
 end
 
+def reverse_each_word(string)
+  array = string.split("")
+  test_array = []
+  array.collect do|string|
+  test_array << string.reverse
+end
+test_array.join("")
+end
