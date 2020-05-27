39010f8518238b5d750ae5fe3254cac875e83389
Robert Defendini <robdefendini@gmail.com>
2020-05-27 22:15:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(string)
-  array = string.split(" ") 
+def reverse_each_word(sentence)
+  array = sentence.split(" ") 
   test_array = []
   array.collect do|string|
-  test_array << string.reverse 
+  test_array << sentence.reverse 
   end
   test_array.join(" ")
 end
 
-
+sentence = ("Hello there, and how are you?")
 reverse_each_word("Hello there, and how are you?")
