69a29f47ee6a8012e55e3b501c28b7b2c3c8251d
Robert Defendini <robdefendini@gmail.com>
2020-05-27 22:02:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,10 @@
 #### My Code ####
+def reverse_each_word_in_string(string)
+original_array = string.split("")
+return_array = []
+original_array.each do|string|
+return_array << string.reverse
+  end
+  return_array.join("")
+end
 
